29f55f6f1d3a4c249e3b2622828f8e00db78d185
jonikumardas <jonikumardas213@gmail.com>
2022-04-18 13:26:40 +0600

    about section has end

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import image from '../img/jon-img.jpg'
+import Footer from '../ShereFolder/Footer/Footer';
 
 const About = () => {
     return (
@@ -11,12 +12,13 @@ const About = () => {
                         
                     </div>
                     <div className="col-md-6">
-                        <h1> About me </h1>
+                        <h1 className='text-info'> About me </h1>
                         <p>This is Joni kumar das.I am  a succassful programmer in bangladesh and I want to be populer in merketplace & want to be spacial icon on others merket place.. I have started my programming juarny since 2020. and my mission 2022 wanna MERNstack devaloper. so I want my mission will be successfull with programming hero. I hope I can do something. Thank you.</p>
 
                     </div>
                 </div>
             </div>
+            <div className='fixed-bottom'><Footer></Footer></div>
             
         </div>
     );
